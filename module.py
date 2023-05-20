c526d95d2da2b8d342e28ac31877fc7624b9302f
Erfan Hes <erfan.datascience@mail.com>
2023-05-21 01:02:12 +0200

    nouvelle branche

--- module.py
+++ module.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 
 def obtenir_temps():
-    return "Hello ! Il est {}.".format(datetime.now().strftime("%H:%M:%S"))
+    return "Helloo ! Il est {}.".format(datetime.now().strftime("%H:%M:%S"))
